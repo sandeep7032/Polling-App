911799876d45aab224f45f4f3be6fbd1cdaa2be1
Sandeep <95558185+sandeep7032@users.noreply.github.com>
2023-06-25 22:19:17 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Polling from './Components/Polling';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Polling />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Polling.jsx
@@ -0,0 +1,70 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+const Polling = () => {
+  const [list, setList] = useState([]);
+  const [page, setPage] = useState(0);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(
+          `https://hn.algolia.com/api/v1/search_by_date?tags=story&page=${page}`
+        );
+        const data = response.data;
+        const newList = data.hits.map((post) => ({
+          title: post.title,
+          url: post.url,
+          created_at: post.created_at,
+          author: post.author,
+        }));
+
+        console.log(newList);
+        setList((prevList) => [...prevList, ...newList]);
+      } catch (error) {
+        console.error("Error fetching posts:", error);
+      }
+    };
+
+    const timer = setInterval(() => {
+      fetchData();
+      setPage((prevPage) => prevPage + 1);
+    }, 10000);
+
+    return () => clearInterval(timer);
+  }, []);
+
+  return (
+    <div>
+      <h1>Polling App</h1>
+      {list.length > 0 && (
+        <table rules="all" style={{ border: "1px solid black" }}>
+          <thead>
+            <tr>
+              <th>Title</th>
+              <th>URL</th>
+              <th>Created At</th>
+              <th>Author</th>
+            </tr>
+          </thead>
+          <tbody>
+            {list.map((post, index) => (
+              <tr key={index}>
+                <td>{post.title}</td>
+                <td>
+                  <a href={post.url} target="_blank">
+                    {post.url}
+                  </a>
+                </td>
+                <td>{post.created_at}</td>
+                <td>{post.author}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      )}
+    </div>
+  );
+};
+
+export default Polling;
